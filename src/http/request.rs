da0829e2bf67d4c7a66e95f8863586572601daac
Gon Gis <g0ng0n.dev@gmail.com>
2022-01-09 11:09:06 +0100

    adding request, and server mods with error handling

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,60 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::str;
+use std::error::Error;
+use std::fmt::{Formatter, Display, Debug, Result as FmtResult};
+
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request{
+    type Error = ParseError;
+
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        match str::from_utf8(buf){
+            Ok(request) => {},
+            Err(e)=> return Err(ParseError::InvalidEncoding),
+        }
+
+        match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)){
+            Ok(request) => {},
+            Err(e)=> return Err(e);
+        }
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidadMethodError,
+
+}
+
+impl Display for ParseError{
+    fn fmt(&self, f: &mut Formatter)-> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+impl Debug for ParseError{
+    fn fmt(&self, f: &mut Formatter)-> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl ParseError {
+    fn message(&self) -> &str{
+        match self {
+            Self::InvalidRequest => "InvalidRequest",
+            Self::InvalidEncoding => "InvalidEncoding",
+            Self::InvalidProtocol => "InvalidProtocol",
+            Self::InvalidadMethodError => "InvalidadMethodError",
+        }
+    }
+}
+
+impl Error for ParseError {}
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,9 @@
+use std::net::TcpListener;
+use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
 pub struct Server{
     address: String,
 }
@@ -11,5 +17,27 @@ impl Server{
 
     pub fn run(self){
         println!("Listening on {}", self.address);
+
+        let listener = TcpListener::bind(&self.address).unwrap();
+
+        loop {
+
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer= [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Receveid a request: {}", String::from_utf8_lossy(& buffer))
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request)=> {},
+                                Err(e) => println!("Error {}", e);
+                            }
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e)
+                    };
+                },
+                Err(e) => println!("Failes to establish a connect: {}", e)
+            }
+        }
     }
 }
